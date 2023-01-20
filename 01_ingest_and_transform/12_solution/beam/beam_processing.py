1de15f3ce0213e8855940a9b2ad5b60d4d5d82ab
Laura Horvath-Perge <laura.perge@gmail.com>
2023-01-20 22:46:07 +0000

    update beam_processing requirements, ExtractAndSumValue transformation, added FormatByRow transformation, uncommented BigQuery write of fixed_windowed_items

--- 01_ingest_and_transform/12_solution/beam/beam_processing.py
+++ 01_ingest_and_transform/12_solution/beam/beam_processing.py
@@ -8,7 +8,6 @@ from apache_beam.io.gcp.pubsub import ReadFromPubSub
 from apache_beam.io.gcp.bigquery import BigQueryDisposition, WriteToBigQuery
 from apache_beam.runners import DataflowRunner
 
-
 # Defining event filter functions.
 def is_item_view(event):
     return event['event'] == 'view_item'
@@ -27,31 +26,38 @@ class ExtractValueFn(beam.DoFn):
       return [element['ecommerce']['purchase']['value']]
 
 class ExtractAndSumValue(beam.PTransform):
-  """A transform to extract key/score information and sum the scores.
+    """A transform to extract key/score information and sum the scores.
   The constructor argument `field` determines whether 'team' or 'user' info is
   extracted.
   """
-  def __init__(self):
-    beam.PTransform.__init__(self)
-    # self.field = field
+    def expand(self, pcoll):  
+        sum_val = (
+            pcoll
+            | beam.Map(lambda elem: (elem['client_id'], elem['ecommerce']['purchase']['value']))
+            | beam.CombinePerKey(sum))
+        return(sum_val)
 
-  def expand(self, pcoll):
-    print('expanding!')
-    return(
-        pcoll
-        | beam.Map(lambda elem: (elem['client_id'], elem['ecommerce']['purchase']['value']))
-        | beam.CombinePerKey(sum))
-
+class FormatByRow(beam.PTransform):
+    """A transform to reformat the data to column name/value format.
+  """
+    def expand(self, pcoll):  
+        row_val = (
+            pcoll
+            | beam.Map(lambda elem: {'user_id': elem[0],
+                                     'summed_value':elem[1]
+            })
+            )
+        return(row_val)
 
 def streaming_pipeline(project, region):
-    topic = "projects/poerschmann-hyp-test2/topics/hyp-pubsub-topic"
-    subscription = "projects/poerschmann-hyp-test2/subscriptions/hyp_test"
+    topic = "projects/{}/topics/hyp-pubsub-topic".format(project)
+    subscription = "projects/{}/subscriptions/hyp_test".format(project)
     item_views_table = "{}:ecommerce_sink.beam_test_views".format(project)
     add_to_carts_table = "{}:ecommerce_sink.beam_test_carts".format(project)
     schema = "event_datetime:DATETIME, event:STRING, user_id:STRING, client_id:STRING, page:STRING, page_previous:STRING, " \
         "item_name:STRING, item_id:STRING, price:STRING, item_brand:STRING, item_category:STRING, item_category_2:STRING, item_category_3:STRING, " \
         "item_category_4:STRING, item_variant:STRING, item_list_name:STRING, item_list_id:STRING, quantity:STRING"
-    bucket = "gs://poerschmann-hyp-test2-ecommerce-events/tmp_dir"
+    bucket = "gs://{}-ecommerce-events/tmp_dir".format(project)
 
     # Defining pipeline options.
     options = PipelineOptions(
@@ -61,7 +67,7 @@ def streaming_pipeline(project, region):
         staging_location="%s/staging" % bucket,
         temp_location="%s/temp" % bucket,
         subnetwork='regions/europe-west1/subnetworks/terraform-network',
-        service_account_email='retailpipeline-hyp@poerschmann-hyp-test2.iam.gserviceaccount.com',
+        service_account_email='retailpipeline-hyp@{}.iam.gserviceaccount.com'.format(project),
         max_num_workers=1
         )
 
@@ -144,10 +150,11 @@ def streaming_pipeline(project, region):
                                                                  accumulation_mode=trigger.AccumulationMode.ACCUMULATING)
                             # | 'Sum per window' >> beam.CombineGlobally(sum)
                             # | 'Log Per Summed Window' >> beam.ParDo(lambda x: LogWindowedElements(x))
-                            #   | 'ExtractAndSumValue' >> ExtractAndSumValue()
+                            | 'ExtractAndSumValue' >> ExtractAndSumValue()
+                            | 'FormatByRow' >> FormatByRow()
                             #   | 'Count' >> beam.transforms.combiners.Count.Globally()
                             # | 'Extract Value' >> beam.ParDo(ExtractValueFn())
-                              | 'Print PColl' >> beam.ParDo(print) # -> working without pre steps!
+                            #  | 'Print PColl' >> beam.ParDo(print) # -> working without pre steps!
                             # | 'Log Windowed' >> beam.Map(lambda x: LogWindowedElements(x))
                             # | 'Count elements by key' >> beam.combiners.Count.PerKey()
                             )
@@ -156,9 +163,9 @@ def streaming_pipeline(project, region):
     test_schema = "user_id:STRING, summed_value:FLOAT"
     test_value_table = "{}:ecommerce_sink.beam_test_value".format(project)
     
-    # fixed_windowed_items | "Write Summed Values To BigQuery" >> WriteToBigQuery(table=test_value_table, schema=test_schema,
-    #                                                                 create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
-    #                                                                 write_disposition=BigQueryDisposition.WRITE_APPEND)
+    fixed_windowed_items | "Write Summed Values To BigQuery" >> WriteToBigQuery(table=test_value_table, schema=test_schema,
+                                                                    create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
+                                                                    write_disposition=BigQueryDisposition.WRITE_APPEND)
 
 
     # Writing the PCollections to two differnt BigQuery tables.
