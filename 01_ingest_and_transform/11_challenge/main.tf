b82c27d60743542c09e119350d55d58d8481fab2
jakobap <jakobpoerschmann@gmail.com>
2023-02-18 20:28:40 +0000

    tf pubsub roles

--- 01_ingest_and_transform/11_challenge/main.tf
+++ 01_ingest_and_transform/11_challenge/main.tf
@@ -125,3 +125,15 @@ resource "google_project_service" "pubsub" {
   service = "pubsub.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_iam_member" "viewer" {
+  project = var.project_id
+  role   = "roles/bigquery.metadataViewer"
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "editor" {
+  project = var.project_id
+  role   = "roles/bigquery.dataEditor"
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
