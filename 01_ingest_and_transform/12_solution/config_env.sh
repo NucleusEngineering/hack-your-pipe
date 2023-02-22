f029757ae5ea3f11935ae08cdbb8ee8941eed974
jakobap <jakobpoerschmann@gmail.com>
2023-02-22 13:36:07 +0100

    env var config files

---
+++ 01_ingest_and_transform/12_solution/config_env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export GCP_PROJECT="<project-id>"
+export GCP_REGION=europe-west1
+export RUN_PROXY_DIR=cloud-run-pubsub-proxy
+export RUN_PROCESSING_DIR=processing-service
+export DATAFLOW_TEMPLATE=beam
+export RUN_INFERENCE_PROCESSING_SERVICE=inf_processing_service
+
